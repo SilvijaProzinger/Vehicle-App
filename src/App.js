3a9fc3af23ad3d7a68592a261bd58a3eb76125be
unknown <silvija.prozinger@gmail.com>
2019-11-18 15:40:54 +0100

    removed old comments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-//import { inject, observer } from 'mobx-react';
-//import { decorate, observable, action, computed } from 'mobx';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-//import store from './stores/CarStore.js';
 import Header from './components/Header.js';
 import Cars from './components/Cars.js';
 import CarDetails from './components/CarDetails.js';
 import './App.css';
-
-/*decorate(store, {
-  cars: observable,
-  filter: action,
-  filteredCars: computed,
-  sortedCars: computed
-})
-*/
 
 class App extends Component {
   render(){
